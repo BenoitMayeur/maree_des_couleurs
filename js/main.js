9d8d01adecb7a9bb80b2ea5e02d5e9a2f2a755af
Benoit Mayeur <benoitmayeur@gmail.com>
2020-12-15 16:56:01 +0100

    main update CSS + modal added
    CSS: blue was not "blue" enough and was too close to the green on some screens. Modal added to remove the alert

--- js/main.js
+++ js/main.js
@@ -52,12 +52,23 @@ function play(color) {
 
 function showMessage(amountOfTimes){
 
-   let answer = confirm(`Vous avez gagné en ${amountOfTimes} coups! 
-   Voulez-vous recommencer?`);
+    modal.style.display = "block";
 
-    if (answer == true) {
+    document.querySelector(".textModal").innerHTML = `Vous avez gagné en ${amountOfTimes} coups! \n Voulez-vous recommencer?`; 
+
+    let divYes = document.getElementsByClassName("modalYes")[0];
+
+    divYes.onclick = function() {
+        modal.style.display = "none";
         restartTheGame();
-    } 
+    }
+
+//    let answer = confirm(`Vous avez gagné en ${amountOfTimes} coups! 
+//    Voulez-vous recommencer?`);
+
+//     if (answer == true) {
+//         restartTheGame();
+//     } 
 
 }
 
@@ -302,4 +313,34 @@ for(let button of listButtons){
 
         play(color);
     })
+}
+
+///////////////////////////////
+
+// Get the modal
+let modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+let btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+let span = document.getElementsByClassName("close")[0];
+
+let divNo = document.getElementsByClassName("modalNo")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks on the button "Non" in the modal, close the modal
+divNo.onclick = function() {
+    modal.style.display = "none";
+  }
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
 }
